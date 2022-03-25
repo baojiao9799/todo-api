91f2f6d134e0830889733f82b7a10200757f8e82
Stephanie Li <baojiao9799@gmail.com>
2022-03-25 13:21:50 -0400

    add user and session models

---
+++ TodoApi/Models/Session.cs
@@ -0,0 +1,8 @@
+namespace TodoApi.Models {
+    public class Session {
+        public Guid Id { get; set; }
+        public string? Token { get; set; }
+        public User User { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
--- TodoApi/Models/Todo.cs
+++ TodoApi/Models/Todo.cs
@@ -6,17 +6,11 @@ namespace TodoApi.Models
     public class Todo : IEntity<Guid>
     {
         public Guid Id { get; set; }
+        public User User { get; set; }
         public string? Title { get; set; }
         [JsonConverter(typeof(JsonStringEnumConverter))]
         public Status Status { get; set; } = Status.Open;
         public DateTime CreationDate { get; set; } = DateTime.UtcNow;
         public DateTime DueDate { get; set; }
-    }
-
-    public enum Status
-    {
-        Open,
-        InProgress,
-        Complete
     }
 }
